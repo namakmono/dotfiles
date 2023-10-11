6f391a9aeace4b70e4caebcb5165e6b62447a46f
namakmono <cct@github.com>
2023-10-11 12:34:23 +0900

    Add keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
--- base settings
+-- base --
 -- tab
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
@@ -6,7 +6,17 @@ vim.opt.shiftwidth = 2
 -- indent
 vim.opt.autoindent = true
 vim.opt.smartindent = true
+-- other
+vim.opt.timeoutlen = 300
 
+-- leader
+vim.g.mapleader = ' '
+
+-- keymap --
+vim.keymap.set({'v', '!'}, 'kj', '<esc>')
+vim.keymap.set({'v', '!'}, 'jk', '<esc>')
+
+-- plugins --
 -- Lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
